fc5e4ad7e398246646dd16aae5e9c3c0cf0f3bd0
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2024-06-19 21:22:03 +0900

    fetch系メソッド追加

--- sqlkun/data/database.py
+++ sqlkun/data/database.py
@@ -13,6 +13,20 @@ class Database:
         self.bot = bot
         self.main: aiosqlite.Connection = conn
 
+    async def fetchrow(self, sql, *place):
+        main = self.main
+        cursor = await main.execute(sql, *place)
+        row = await cursor.fetchone()
+        await cursor.close()
+        return row
+    
+    async def fetch(self, sql, *place):
+        main = self.main
+        cursor = await main.execute(sql, *place)
+        rows = await cursor.fetchall()
+        await cursor.close()
+        return rows
+
     async def execute(self, sql, parameters: tuple = None):
         cursor = await self.main.cursor()
         await cursor.execute(sql, parameters)
